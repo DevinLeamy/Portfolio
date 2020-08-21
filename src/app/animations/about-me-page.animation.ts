c912d2a5c4a4b17e4942990932e7325161b8acb5
Devin Leamy <devinleamy@gmail.com>
2020-08-21 09:40:30 -0600

    polyfills and animations

--- src/app/animations/about-me-page.animation.ts
+++ src/app/animations/about-me-page.animation.ts
@@ -1,10 +1,12 @@
 import { trigger, state, style, transition, animate, stagger, query, animateChild, animation, group, keyframes } from "@angular/animations"
 
 
+let schoolAnimation = query(".school-text, .school-icon", animate(400))
+
 let iconAnimation = animate(400, keyframes([
         style({width: "10px", height: "10px", opacity: 0, margin: 'auto'}),
         style({width: '100px', height: "100px", opacity: 1}),
-        style({})
+        style({width: '60px', height: "60px", opacity: 1})
 ]))
 
 let traitsAnimation = query(".traits-container, .traits-text, .traits-icon",  
@@ -14,7 +16,7 @@ let traitsAnimation = query(".traits-container, .traits-text, .traits-icon",
                                 style({opacity: 1, transform: "translateY(0px)"})
                         ))
                 ),
-                query(".trait-container", stagger(400, animate(400, keyframes([
+                query(".trait-container", stagger(400, animate(800, keyframes([
                         style({borderBottom: '4px solid hotpink'}),
                         style({borderBottom: '4px solid rgb(85, 84, 84)'})
                 ])))),
@@ -31,24 +33,14 @@ let initialPositions = [
                         height: 0
                 })
         ]),
-        query(".school-icon, .school-text-container, .trait-icon, .link, .middle-top-header, .interest-header, .interest-body", style({
-                opacity: 0
-        })),
-        query(".trait-container", style({
-                borderBottom: "4px solid transparent"
-        })),
-        query(".trait-text", style({
-                opacity: 0,
-                transform: "translateY(100px)"
-        })),
-        query(".header", style({
-                opacity: 0
-        })),
+        query(".trait-icon, .link, .middle-top-header, .interest-header, .interest-body", style({ opacity: 0 })),
+        query(".school-icon", style({opacity:0, transform: "translateX(-200px)"})),
+        query(".school-text", style({opacity:0, transform: "translateX(200px)"})),
+        query(".trait-container", style({ borderBottom: "4px solid transparent" })),
+        query(".trait-text", style({ opacity: 0, transform: "translateY(100px)" })),
+        query(".header", style({ opacity: 0 })),
         query(".middle-container", [
-                style({
-                        transform: "translateX(1600px)",
-                        opacity: 0
-                })
+                style({ transform: "translateX(800px)", opacity: 0 })
         ])
 ]
 
@@ -59,14 +51,33 @@ let containerAnimation = query(".right-container, .left-container, .middle-conta
 let headerAnimation = query(".header", animate("200ms ease", style({
                 opacity: 1
         })))
+let linkAnimation = query(".link, .link-text",
+        group([
+                animate(1000, keyframes([
+                        style({opacity: 1, filter: "blur(9px)"}),
+                        style({filter: "blur(0px)"})
+                ])),
+                // animate(700, keyframes([
+                //         style({fontSize: "60px", color: "rgb(56, 55, 54)"}),
+                //         style({fontSize: "70px", color: "rgb(162, 186, 250)"}),
+                //         style({fontSize: "50px", color: "rgb(56, 55, 54)"})
+                // ]))
+        ])
+)
 let pageSlide = trigger("aboutMePageAnimation", [
         transition(":enter", [
                 initialPositions[0],
                 initialPositions[1],
                 initialPositions[2],
                 initialPositions[3],
+                initialPositions[4],
+                initialPositions[5],
+                initialPositions[6],
+                initialPositions[7],
                 containerAnimation,
                 headerAnimation,
+                linkAnimation,
+                schoolAnimation,
                 traitsAnimation
         ])
 ])
